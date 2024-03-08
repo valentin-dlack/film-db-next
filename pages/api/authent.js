df7debbf7de891f5f58b8b7a6da18b02a43f0a43
valentin dautrement <valentin.dautrement@protonmail.com>
2024-03-08 09:18:15 +0100

    base auth

---
+++ pages/api/authent.js
@@ -0,0 +1,5 @@
+import { ConfigService } from "../../services/config.service";
+
+export default async function handler(req, res) {
+    res.json({ status: 200, data: { message: "This page is not implemented yet" } });
+}

abc0742a733074d2953bbd23857375ccaa5582bc
valentin dautrement <valentin.dautrement@protonmail.com>
2024-03-24 21:26:37 +0100

    merge

--- pages/api/auth/profile.js
+++ pages/api/auth/profile.js
@@ -1,9 +1,15 @@
 import { verifyToken } from "../../../src/middleware/verifyToken";
+import clientPromise from "/lib/mongodb";
 
 export default async function handler(req, res) {
     // Check if the user is authorized
-    await verifyToken(req, res);
+    const token = await verifyToken(req, res);
 
-    // Return the user's profile
-    return res.status(200).json({ message: "User profile" });
+    // Connect to MongoDB
+    const client = await clientPromise;
+    const db = client.db("ynov-cloud");
+
+    // Retrieve the user from the database
+    const user = await db.collection("users").findOne({ _id: req.userId });
+
 }
--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -27,7 +27,7 @@ export async function verifyToken(req, res) {
         req.headers.authorization = newToken;
       }
   
-      return Response;
+      return Response.ok;
     } catch (error) {
         switch (error.message) {
             case 'jwt must be provided':

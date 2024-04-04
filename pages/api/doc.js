20c149dba31f6ca0d4e5ef466d70450ff6e34749
valentin dautrement <valentin.dautrement@protonmail.com>
2024-04-04 15:30:28 +0200

    doc version

--- pages/api/doc.js
+++ pages/api/doc.js
@@ -3,7 +3,7 @@ import { withSwagger } from 'next-swagger-doc';
 const swaggerHandler = withSwagger({
   openApiVersion: '3.0.0',
   title: 'Next.js API Movie Database',
-  version: '1.0.0',
+  version: '1.1.0',
   apiFolder: 'pages/api',
 
 });
